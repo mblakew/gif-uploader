c37b9992241261c15e175eb605015bab0aa9c45d
Blake Williams <michaelbwilliams100@gmail.com>
2020-11-28 16:21:03 -0500

    Add template shell script for ease of acces, rename main.py to mac2imgur.py

---
+++ script.sh
@@ -0,0 +1,7 @@
+# The following script resides in my default directory
+# for easy access by simply opening a terminal.
+
+# #!/bin/sh
+# source {path-to-project}/venv/bin/activate
+# python {path-to-project}/mac2imgur.py
+# deactivate
