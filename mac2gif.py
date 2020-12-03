0b090ba293ba10c60674317977f6a3ff8c297941
Blake Williams <michaelbwilliams100@gmail.com>
2020-12-03 11:21:24 -0500

    File size check to avoid failed Imgur upload

--- mac2gif.py
+++ mac2gif.py
@@ -1,25 +1,19 @@
-import utility
-import pyperclip as clip
+import utility as util
+
+
+def upload(file_path):
+    if util.get_file_size(file_path) < 10:
+        print("\nUploading to Imgur...")
+        link = util.upload_to_imgur(util.get_file(file_path))
+    else:
+        print("\nFile too large for Imgur, uploading to Giphy...")
+        link = util.upload_to_giphy(util.get_file(file_path))
+
+    util.copy_to_clipboard(link)
+
 
 while True:
-    file_path = input("\nPlease enter the path to the file you would like to upload: ")
-
-    
-   
-    print("\nAttempting Imgur upload...")
-    link = utility.upload_to_imgur(file_path)
-
-    if not link:
-        print("\nUpload to Imgur failed, uploading to Giphy...")
-        link = utility.upload_to_giphy(file_path)
-
-    if link:
-        clip.copy(link)
-        print("\nThe link should be on your clipboard! If not, copy it here: " + link)
-        exit()
-    else:
-        print(
-            "\nSomething went wrong. The file might have been too large, or the daily upload limit " +
-            "to Giphy may have been met check json for more details"
-        )
-        continue
+    file_path = input(
+        "\nPlease enter the path to the file you would like to upload: ")
+    # exit()
+    upload(file_path)
--- utility.py
+++ utility.py
@@ -1,7 +1,28 @@
 import requests
 from decouple import config
+import pyperclip as clip
+from pathlib import Path
 
 
+# Returns file size in bytes
+def get_file_size(file_path):
+    return (Path(file_path).stat().st_size / 1000000)
+
+
+# Copies link to clipboard and exits. Prints error message if link is empty
+def copy_to_clipboard(link):
+    if link:
+        clip.copy(link)
+        print("\nThe link should be on your clipboard! If not, copy it here: " + link)
+        exit()
+    else:
+        print(
+            "\nSomething went wrong. The file might have been too large, or the daily upload limit " +
+            "to Giphy may have been met check json for more details"
+        )
+
+
+# Returns None if file could not be opened
 def get_file(path):
     file = ""
     try:
@@ -13,8 +34,8 @@ def get_file(path):
 
 # Uploads file to imgur and returns link if successful (status code 200).
 # If unsuccessful, returns None
-def upload_to_imgur(path):
-    file = get_file(path)
+def upload_to_imgur(file):
+    # file = get_file(path)
     url = "https://api.imgur.com/3/image"
     CLIENT_ID = config("CLIENT_ID")
 
@@ -22,7 +43,8 @@ def upload_to_imgur(path):
     files = [("image", file)]
     headers = {"Authorization": "Client-ID {0}".format(CLIENT_ID)}
 
-    res = requests.request("POST", url, headers=headers, data=payload, files=files)
+    res = requests.request("POST", url, headers=headers,
+                           data=payload, files=files)
     if res.status_code != 200:
         # print("Something went wrong, response: ")
         # print(res.json())
@@ -31,15 +53,15 @@ def upload_to_imgur(path):
     return res.json()["data"]["link"]
 
 
-def upload_to_giphy(path):
-    file = get_file(path)
+def upload_to_giphy(file):
+    # file = get_file(path)
     api_endpoint = "http://api.giphy.com/v1/gifs"
     upload_endpoint = "http://upload.giphy.com/v1/gifs"
     GIPHY_ID = config("GIPHY_ID")
     USERNAME = config("GIPHY_USERNAME")
     params = {
-    "api_key": GIPHY_ID,
-    "username": USERNAME
+        "api_key": GIPHY_ID,
+        "username": USERNAME
     }
 
     res = requests.post(upload_endpoint, params=params, files={'file': file})
@@ -50,7 +72,7 @@ def upload_to_giphy(path):
     # print("\nJOINED_ENDPOINT: \n ")
     joined_endpoint = '/'.join((api_endpoint, gif_id))
     # print(joined_endpoint)
-    
+
     res = requests.get(joined_endpoint, params=params)
     res_data = res.json()
     # print(res_data)
